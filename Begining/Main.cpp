1b9bb952b6807d849b558901d35bfcf3d1cc1023
Ильин Павел <Paxamet@gmail.com>
2021-01-09 17:23:37 +0300

    start

---
+++ Begining/Main.cpp
@@ -0,0 +1,68 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Settings.h";
+
+using namespace sf;
+
+int main()
+{
+	WindowSettings WinSettings;
+	RenderWindow window(sf::VideoMode(WinSettings.WindowWidth, WinSettings.WindowWidth), "Begining by Paxamet");
+	
+	Image HeroImage;
+	HeroImage.loadFromFile("images/hero.png");
+	HeroImage.createMaskFromColor(Color(0,255,0));
+
+	Texture HeroTexture;
+	HeroTexture.loadFromImage(HeroImage);
+
+	Sprite HeroSprite;
+	HeroSprite.setTexture(HeroTexture);
+	HeroSprite.setTextureRect(IntRect(0, 0, 64, 64));
+	HeroSprite.setPosition(WinSettings.WindowWidth / 2, WinSettings.WindowHeigh / 2);
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed) {
+				window.close();
+				std::cout << "---shutdown program";
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::W)) 
+			{
+				HeroSprite.move(0, -0.5);
+				HeroSprite.setRotation(0);
+			}
+			if (Keyboard::isKeyPressed(Keyboard::S))
+			{
+				HeroSprite.move(0, 0.5);
+				HeroSprite.setRotation(180);
+			}
+			if (Keyboard::isKeyPressed(Keyboard::A))
+			{
+				HeroSprite.move(-0.5, 0);
+				HeroSprite.setRotation(270);
+			}
+			if (Keyboard::isKeyPressed(Keyboard::D))
+			{
+				HeroSprite.move(0.5, 0);
+				HeroSprite.setRotation(90);
+			}
+
+			if (Mouse::isButtonPressed(Mouse::Left))
+			{
+				HeroSprite.setPosition(Vector2f(Mouse::getPosition()));
+			}
+
+			//std::cout << "---Hero pos (x: " << HeroSprite.getPosition().x << ", y: " << HeroSprite.getPosition().y << ")\n";
+		}
+		window.clear();
+		window.draw(HeroSprite);
+		window.display();
+	
+	}
+	return 0;
+}
---
+++ Begining/Settings.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+struct WindowSettings
+{
+	int WindowWidth = 800;
+	int WindowHeigh = 600;
+};
